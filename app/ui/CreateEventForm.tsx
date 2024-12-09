bc549f5398cb6a699239dedaf3fc1e182bf54135
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-12-09 19:48:34 +0100

    Render calendar from Monday in European locales

--- app/ui/CreateEventForm.tsx
+++ app/ui/CreateEventForm.tsx
@@ -111,6 +111,7 @@ export function CreateEventForm({
               setDateRange(range || { from: undefined, to: undefined })
             }
             selected={dateRange}
+            weekStartsOn={tryGetFirstDayOfTheWeek()}
           />
         </div>
         <button
@@ -132,4 +133,31 @@ export function CreateEventForm({
   );
 }
 
-export default CreateEventForm;
+function tryGetFirstDayOfTheWeek() {
+  const language = getNavigatorLanguage();
+  const locale = new Intl.Locale(language);
+
+  type WeekInfo = { firstDay: number } | undefined;
+
+  const weekInfo: WeekInfo =
+    "getWeekInfo" in locale
+      ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        ((locale as any).getWeekInfo() as WeekInfo)
+      : (locale as unknown as { weekInfo: WeekInfo }).weekInfo;
+
+  return weekInfo?.firstDay
+    ? // react-day-picker is American and treats Sunday as 0 not 7
+      ((weekInfo.firstDay % 7) as 0 | 1 | 2 | 3 | 4 | 5 | 6)
+    : undefined;
+}
+
+function getNavigatorLanguage() {
+  if (navigator.languages && navigator.languages.length) {
+    return navigator.languages[0];
+  } else {
+    return ((navigator as { userLanguage?: string }).userLanguage ||
+      navigator.language ||
+      (navigator as { browserLanguage?: string }).browserLanguage ||
+      "en") as string;
+  }
+}
