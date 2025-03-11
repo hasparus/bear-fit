a491c96a3ab7e12335fcd7eb0ab22d75b727c1ab
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-11 15:02:13 +0100

    Desaturate others on hover

--- app/ui/EventDetails.tsx
+++ app/ui/EventDetails.tsx
@@ -383,13 +383,13 @@ function AvailabilityGridCell({
     <button
       aria-label={day.toLocaleDateString(undefined, { dateStyle: "full" })}
       className={cn(
-        "group flex items-center justify-center rounded-md size-10 select-none hover:border-neutral-200 bg-neutral-100 hover:border-2 relative transition border-transparent",
+        "group flex items-center justify-center rounded-md size-10 select-none hover:border-neutral-200 bg-neutral-100 hover:border-2 relative transition-all border-transparent",
         (currentUserAvailable || hoveredUser === "available") &&
           "border-neutral-200 border-4",
         currentUserAvailable &&
           hoveredUser === "available" &&
           "border-neutral-200 border-[6px]",
-        hoveredUser === "unavailable" && "opacity-60",
+        hoveredUser === "unavailable" && "opacity-60 saturate-25",
       )}
       style={{
         color: fill > 0.5 ? "white" : "black",
