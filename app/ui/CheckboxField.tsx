11deabe45b379f645000a82cce99488357ac65f7
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-21 01:07:01 +0100

    Fix children

--- app/ui/CheckboxField.tsx
+++ app/ui/CheckboxField.tsx
@@ -3,7 +3,11 @@ import { cn } from "./cn";
 export interface CheckboxFieldProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export function CheckboxField({ className, ...rest }: CheckboxFieldProps) {
+export function CheckboxField({
+  className,
+  children,
+  ...rest
+}: CheckboxFieldProps) {
   return (
     <div className={cn("field-row flex items-center", className)}>
       <input type="checkbox" {...rest} />
@@ -11,7 +15,7 @@ export function CheckboxField({ className, ...rest }: CheckboxFieldProps) {
         htmlFor="nerd-mode"
         className="!cursor-pointer text-xs [input:checked+&]:after:!content-['x'] after:!bg-none  after:absolute after:!left-[-16.66px] after:!top-[3px] py-1 w-full !leading-[14px]"
       >
-        Nerd Mode
+        {children}
       </label>
     </div>
   );
