eb14bfdad9fb167b814ec3e9c0f7fe29b93794ae
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-06-09 16:17:05 +0200

    fix backticks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ BRANCH_NAME=$(echo "$BRANCH_NAME" | tr '/' '-' | tr '[:upper:]' '[:lower:]')
 MAX_PREVIEW_BRANCH_NAME_LENGTH=25
 COMMIT_HASH=${COMMIT_HASH:-$(git rev-parse --short HEAD)}
 
-if [ "$BRANCH_NAME" != "main" ] && [ ${`#BRANCH_NAME`} -gt $MAX_PREVIEW_BRANCH_NAME_LENGTH ]; then
+if [ "$BRANCH_NAME" != "main" ] && [ ${#BRANCH_NAME} -gt $MAX_PREVIEW_BRANCH_NAME_LENGTH ]; then
   # Take first 20 chars of branch name + hyphen + first 4 chars of commit hash = 25 total
   BRANCH_NAME="$(echo "$BRANCH_NAME" | cut -c1-20)-$(echo "$COMMIT_HASH" | cut -c1-4)"
 fi
